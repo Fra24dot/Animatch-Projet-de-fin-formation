8c052ec9a6584989ab8e746e0e49337457c83684
fra <frarusso24@live.it>
2026-05-27 14:09:25 +0200

    feat/auth:implement of Authservice and controller

---
+++ Animatch.Api/Mappers/UserMapper.cs
@@ -0,0 +1,30 @@
+﻿using Animatch.Api.Dtos.Response;
+using Animatch.Domain.Entities;
+
+namespace Animatch.Api.Mappers
+{
+    public static class UserMapper
+    {
+        public static RegisterUserResponseDto ToRegisterResponseDto(this User user)
+        {
+            return new RegisterUserResponseDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                AccountType = user.AccountType.ToString()
+            };
+        }
+
+        public static LoginResponseDto ToLoginResponseDto(this User user, string token)
+        {
+            return new LoginResponseDto
+            {
+                Token = token,
+                Email = user.Email,
+                AccountType = user.AccountType.ToString()
+            };
+        }
+    }
+}
