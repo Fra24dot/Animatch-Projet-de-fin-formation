981a96308135a154269ecda51a5112a2d9f0d4cb
fra <frarusso24@live.it>
2026-06-18 16:41:29 +0200

    feature/websocket: implement of chatHub

---
+++ Animatch.Core/Interfaces/Repositories/Tools/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using Animatch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animatch.Core.Interfaces.Repositories.Tools
+{
+    public interface IMessageRepository
+    {
+        Task<Match?> GetMatchWithDetailsAsync(Guid matchId);
+        Task AddMessageAsync(Message message);
+        Task SaveChangesAsync();
+    }
+}
---
+++ Animatch.Infra/Repositories/Tools/MessageRepository.cs
@@ -0,0 +1,31 @@
+﻿using Animatch.Core.Interfaces.Repositories.Tools;
+using Animatch.Domain.Entities;
+using Animatch.Infrastructure.Database.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animatch.Infrastructure.Repositories.Tools
+{
+    public class MessageRepository(AnimatchDbContext _context) : IMessageRepository
+    {
+        
+        public async Task<Match?> GetMatchWithDetailsAsync(Guid matchId)
+        {
+            return await _context.Matches
+                .Include(m => m.Dog) 
+                .FirstOrDefaultAsync(m => m.Id == matchId);
+        }
+
+        public async Task AddMessageAsync(Message message)
+        {
+            await _context.Messages.AddAsync(message);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
